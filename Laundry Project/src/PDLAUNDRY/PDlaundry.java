6658d8b13b83cf44078f823f9ebd07c62cf20fc9
sakafana <150043655+sakafana@users.noreply.github.com>
2024-11-20 08:11:02 +0700

    Add files via upload

---
+++ Laundry Project/src/PDLAUNDRY/PDlaundry.java
@@ -0,0 +1,14 @@
+
+package PDLAUNDRY;
+
+
+public class PDlaundry {
+
+    
+    public static void main(String[] args) {
+        Login ya = new Login();
+        ya.setVisible(true);
+    }
+    
+    
+}
